a87dab6e4cb88aedfed84728b0c0e1d0f997da96
Maël Martin <mael.martin@fulll.fr>
2026-04-20 14:05:46 +0200

    config: add nitro compat date

--- apps/app/vite.config.ts
+++ apps/app/vite.config.ts
@@ -7,7 +7,7 @@ import { nitroV2Plugin } from "@tanstack/nitro-v2-vite-plugin";
 
 export default defineConfig({
 	plugins: [
-		nitroV2Plugin({ preset: "vercel" }),
+		nitroV2Plugin({ preset: "vercel", compatibilityDate: "2026-04-20" }),
 		viteTsConfigPaths({ projects: ["./tsconfig.json"] }),
 		tailwindcss(),
 		tanstackStart(),
